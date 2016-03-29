1feab212706d18b7240d59f727e6bcf13480fb7c
zachsand <zachsand@utexas.edu>
2016-03-29 13:53:38 -0500

    Added no_event_startup page based on plans from pxrogers/UTimeline#2, haven't add a listener for the button yet.

--- app/src/main/java/net/srogers/utimeline/MainActivity.java
+++ app/src/main/java/net/srogers/utimeline/MainActivity.java
@@ -3,11 +3,34 @@ package net.srogers.utimeline;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import net.srogers.utimeline.model.Timeline;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private Timeline mTimeline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mTimeline = new Timeline();
+
+        if (mTimeline.hasEvents()) {
+            setContentView(R.layout.activity_main);
+        } else {
+            setContentView(R.layout.no_event_startup);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mTimeline.hasEvents()) {
+            setContentView(R.layout.activity_main);
+        } else {
+            setContentView(R.layout.no_event_startup);
+        }
     }
 }
--- app/src/main/java/net/srogers/utimeline/model/Timeline.java
+++ app/src/main/java/net/srogers/utimeline/model/Timeline.java
@@ -1,16 +1,30 @@
 package net.srogers.utimeline.model;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import net.srogers.utimeline.R;
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by steven on 3/28/16.
+ * Timeline class to hold logic about as user's personal UTimeline.
  */
 public class Timeline {
     private List<UTimelineEvent> events;
 
     public Timeline() {
         events = new ArrayList<>();
+    }
+
+    /**
+     * Checks to see whether this Timeline has any events added to it.
+     *
+     * @return boolean, false if there are no UTimelineEvent(s) in events list, true otherwise
+     */
+    public boolean hasEvents() {
+        return !events.isEmpty();
     }
 
     public void addEvent(UTimelineEvent e) {
